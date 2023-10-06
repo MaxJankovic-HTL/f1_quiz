769b08333c197e39f2e0e2ac4ae922efbc70054b
BaptistSchopfhauser-HTL <104189622+BaptistSchopfhauser-HTL@users.noreply.github.com>
2023-10-06 14:57:50 +0200

    little updates (Navbar integriert)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
-  <div class="container text-center">
-    <div class="my-4 d-flex justify-content-center">
-      <RouterLink to="/" class="nav-link me-3" active-class="border-bottom border-3 border-primary"
-        >Home</RouterLink
-      >
-      <RouterLink to="/about" class="nav-link" active-class="border-bottom border-3 border-primary">
-        About
-      </RouterLink>
-    </div>
-
-    <div class="d-flex justify-content-center"><RouterView /></div>
-    <BreakPoints></BreakPoints>
-  </div>
+  <NavBar></NavBar>
+  <RouterView></RouterView>
 </template>
