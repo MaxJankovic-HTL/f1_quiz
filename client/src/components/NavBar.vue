f386d7f32154c659df8c4c002f8cecdb187baa05
BaptistSchopfhauser-HTL <104189622+BaptistSchopfhauser-HTL@users.noreply.github.com>
2023-10-11 09:53:43 +0200

    Update NavBar.vue

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <nav style="background-color: #e10600" class="navbar navbar-expand-lg bg-red rounded-4 mx-2 mt-2">
+  <nav style="background-color: #e10600" class="navbar navbar-dark bg-red rounded-4 mx-2 mt-2">
     <div class="container-fluid">
       <a class="navbar-brand mx-3" href="#">F1 QUIZ</a>
       <button
@@ -20,6 +20,24 @@
           <li class="nav-item">
             <a class="nav-link active mx-3" aria-current="page" href="#">Home</a>
           </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle mx-3"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Quiz
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Easy</a></li>
+              <li><a class="dropdown-item" href="#">Medium</a></li>
+              <li><a class="dropdown-item" href="#">Hard</a></li>
+            </ul>
+          </li>
           <li class="nav-item">
             <a class="nav-link mx-3" href="#">About</a>
           </li>
@@ -29,4 +47,8 @@
   </nav>
 </template>
 
-<style></style>
+<style>
+.navbar-toggler-icon {
+  border-width: 10px;
+}
+</style>
