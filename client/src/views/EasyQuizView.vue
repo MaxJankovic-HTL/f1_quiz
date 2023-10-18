aaa5e34d557aa78256b138d3b944854edc42b4e3
BaptistSchopfhauser-HTL <104189622+BaptistSchopfhauser-HTL@users.noreply.github.com>
2023-10-18 11:18:14 +0200

    easy quiz hat noch fehler

--- client/src/views/EasyQuizView.vue
+++ client/src/views/EasyQuizView.vue
@@ -1,15 +1,30 @@
-<script setup>
+<!-- <script setup>
 import data from '../../../server/f1_questions.json';
 
-// function randomQuestion() {
-//   const easyData = data.filter((item) => item.difficulty === 'easy');
-//   const randomIndex = Math.floor(Math.random() * easyData.length);
-//   return easyData[randomIndex];
-// }
+function randomQuestion() {
+  const easyData = data.filter((item) => item.questions.difficulty === 'easy');
+  const randomIndex = Math.floor(Math.random() * easyData.length);
+  return easyData[randomIndex];
+}
 </script>
 
 <template>
   <div class="container text-center mt-4">
-    <p>{{ data }}</p>
+    <p>{{ randomQuestion().question }}</p>
+  </div>
+</template> -->
+
+<script setup>
+import { json } from '@vue/composition-api';
+
+const questions = json.parseFile('./data.json');
+const easyQuestions = questions.filter((question) => question.difficulty === 'easy');
+</script>
+
+<template>
+  <div class="container text-center mt-4">
+    <p v-for="question in easyQuestions" :key="question.id">
+      {{ question.question }}
+    </p>
   </div>
 </template>
