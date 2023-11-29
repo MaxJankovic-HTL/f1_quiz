e1ba17c29fd702cda77596a43d6df349845edec4
BaptistSchopfhauser-HTL <104189622+BaptistSchopfhauser-HTL@users.noreply.github.com>
2023-11-29 09:51:21 +0100

    update

--- client/src/views/QuizView.vue
+++ client/src/views/QuizView.vue
@@ -1,63 +1,66 @@
 <script setup></script>
 
 <template>
-  <section class="section py-5 text-center">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">fast faster F1</h1>
-        <p class="lead text-muted">
-          Something short and leading about the collection below—its contents, the creator, etc.
-          Make it short and sweet, but not too short so folks don’t simply skip over it entirely.
-        </p>
-      </div>
-    </div>
-  </section>
-  <div class="container">
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <div class="card h-100 text-center">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">EASY QUIZ</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural lead-in to additional
-              content. This content is a little bit longer.
-            </p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
+  <div class="site">
+    <section class="section py-5 text-center">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="fw-light">fast faster F1</h1>
+          <p class="lead text-muted">
+            Something short and leading about the collection below—its contents, the creator, etc.
+            Make it short and sweet, but not too short so folks don’t simply skip over it entirely.
+          </p>
         </div>
       </div>
+    </section>
 
-      <div class="col">
-        <div class="card h-100 text-center">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">MEDIUM QUIZ</h5>
-            <p class="card-text">
-              This card has supporting text below as a natural lead-in to additional content.
-            </p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+    <div class="container mt-4">
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card h-100 text-center">
+            <img src="../../public/images/question images/" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">EASY QUIZ</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural lead-in to additional
+                content. This content is a little bit longer.
+              </p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">> go there ></small>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col">
-        <div class="card h-100 text-center">
-          <img src="..." class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">HARD QUIZ</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural lead-in to additional
-              content. This card has even longer content than the first to show that equal height
-              action.
-            </p>
+        <div class="col">
+          <div class="card h-100 text-center">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">MEDIUM QUIZ</h5>
+              <p class="card-text">
+                This card has supporting text below as a natural lead-in to additional content.
+              </p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">> go there ></small>
+            </div>
           </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+
+        <div class="col">
+          <div class="card h-100 text-center">
+            <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">HARD QUIZ</h5>
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural lead-in to additional
+                content. This card has even longer content than the first to show that equal height
+                action.
+              </p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">> go there ></small>
+            </div>
           </div>
         </div>
       </div>
@@ -67,6 +70,11 @@
 
 <style>
 .section {
-  background-color: gray;
+  background-color:whitesmoke;
+}
+
+.site {
+  background-color:azure;
+  padding-bottom: 100%;
 }
 </style>
