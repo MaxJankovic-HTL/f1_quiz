616ff773fe496047faa1e6939a659dee804fbd5c
BaptistSchopfhauser-HTL <104189622+BaptistSchopfhauser-HTL@users.noreply.github.com>
2023-11-29 10:43:40 +0100

    Update QuizView.vue

--- client/src/views/QuizView.vue
+++ client/src/views/QuizView.vue
@@ -4,16 +4,16 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <div class="site">
-    <section class="section py-5 text-center">
+    <section class="section py-2 text-center shadow-lg">
       <div class="row py-lg-5">
-        <div class="col-lg-6 col-md-8 mx-auto">
+        <div class="col-lg-6 col-md-8 mx-auto mt-2">
           <h1 class="fw-bold">FAST ➔ FASTER ➔ F1</h1>
           <p class="lead text-muted"></p>
         </div>
       </div>
     </section>
 
-    <div class="container">
+    <div class="container mt-5">
       <div class="row row-cols-1 row-cols-md-3 g-4">
         <RouterLink class="col w-25 nav-link mx-5" to="/easy-quiz">
           <div class="card h-100 text-center">
@@ -50,6 +50,9 @@ import { RouterLink } from 'vue-router';
 </template>
 
 <style>
+.section {
+}
+
 .site {
   background-color: f7f4f1;
   padding-bottom: 25%;
